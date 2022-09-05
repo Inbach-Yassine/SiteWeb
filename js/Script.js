65d37e728ecd8b7ad21aeb215766a6685d4c2505
Inbach-Yassine <yassine10.inba@gmail.com>
2022-09-05 22:52:34 +0200

    Coding

--- js/Script.js
+++
@@ -1,7 +0,0 @@
-let Menu = document.querySelector('#menu-bar');
-let Navbar = document.querySelector('.nav-bar');
-
-Menu.onclick = () =>{
-    Menu.classList.toggle('fa-times');
-    Navbar.classList.toggle('active');
-}
